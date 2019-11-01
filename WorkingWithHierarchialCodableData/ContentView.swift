e7dd3751e8b3ffa207e6a647423fb895b1bab6ef
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-01 13:32:45 +0200

    init

--- WorkingWithHierarchialCodableData/ContentView.swift
+++ WorkingWithHierarchialCodableData/ContentView.swift
@@ -10,7 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+            
+            // More code to come
+        }
     }
 }
 
